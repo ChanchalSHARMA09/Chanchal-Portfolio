d16ad5fba2af05085b30915c2730705d9d65ce96
chanchal <chanchal.2312051@gmail.com>
2026-04-08 11:11:34 +0530

    added proper css

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,8 +1,25 @@
+
+import Navbar from '@/components/Navbar'
+import HeroSection from '@/components/HeroSection'
+import AboutSection from '@/components/AboutSection'
+import SkillsSection from '@/components/SkillsSection'
+import ProjectsSection from '@/components/ProjectsSection'
+import EducationSection from '@/components/EducationSection'
+import ContactSection from '@/components/ContactSection'
+
 function App() {
   return (
-    <div>
-      
-    </div>
+   
+      <div className="min-h-screen bg-background text-foreground">
+        <Navbar />
+        <HeroSection />
+        <AboutSection />
+        <SkillsSection />
+        <ProjectsSection />
+        <EducationSection />
+        <ContactSection />
+      </div>
+  
   )
 }
 
